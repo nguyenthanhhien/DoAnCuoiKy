5576e34e1f49ee4c6a6c689c99f79469964088c2
Kieukhanh <nguyenkieukhanh111@gmail.com>
2017-01-04 20:09:31 +0700

    update ảnh v2

--- AppChatRealTime/routes/index.js
+++ AppChatRealTime/routes/index.js
@@ -2,21 +2,46 @@ var express = require('express');
 var passport = require('passport');
 var router = express.Router();
 var multer= require('multer');
-var upload= multer({dest:'public/img/'})
 
-//router.get('/', function(req, res, next) {
-//  res.render('index', { title: 'Express' });
-//});
+var storage =   multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './public/img');
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.fieldname + '-' + Date.now() + ".png");
+  }
+});
+var upload = multer({storage : storage}).single('userPhoto');
+
+router.get('/main',function(req,res){
+      res.sendFile(__dirname + "/main");
+});
+
+router.post('/api/photo',function(req,res){
+    upload(req,res,function(err) {
+        if(err) {
+            return res.end("Error uploading file.");
+        }
+        res.render('profile', {layout:false, message: req.flash('loginMessage'),messagesignup: req.flash('signupMessage')});
+        res.end("File is uploaded");
+
+    });
+});
+
+// router.listen(3000,function(){
+//     console.log("Working on port 3000");
+// });
+
 
 router.get('/login', function(req, res, next) {
   res.render('login', {layout:false, message: req.flash('loginMessage'),messagesignup: req.flash('signupMessage')});
 });
 
-router.post('/profile',upload.any(), function(req, res, next)
-  {
-    console.log("test ham");
-    res.send(req.files);
-  });
+// router.post('/profile',upload.any(), function(req, res, next)
+//   {
+//     console.log("test ham");
+//     res.send(req.files);
+//   });
 
 router.get('/', isLoggedIn, function(req, res) {
   res.render('profile', { user: req.user});
