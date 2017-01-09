3b216127b8bf5b7eadcaca4435e716c6e42c65c5
nguyenthanhhien <nthhien79@gmail.com>
2017-01-10 00:28:08 +0700

    Update database.js
    change link database

--- AppChatRealTime3/config/database.js
+++ AppChatRealTime3/config/database.js
@@ -1,6 +1,4 @@
 module.exports = {
-  url: 'mongodb://localhost/appchat',
-//=======
-//  url: 'mongodb://qltk:12345@ds127958.mlab.com:27958/taikhoan',
-//>>>>>>> 91aa998efca25dccf0eff20c5c21ffb5b19966c7
+//   url: 'mongodb://localhost/appchat',
+ url: 'mongodb://qltk:12345@ds127958.mlab.com:27958/taikhoan',
 };
